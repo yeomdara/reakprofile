9c290053c78c6dc35eb30fac9587e267c8e4863a
yeomdara <108196667+yeomdara@users.noreply.github.com>
2022-07-03 11:25:59 +0000

    Deploying to gh-pages from  @ 4d9a9f8bda5956cce1689a812bb03de75607a7cb 🚀

---
+++ _next/static/AeL6bIiestJJWWH-3_yDs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/868-20da3b41919cd005.js","static/css/e082ac7cbc521954.css","static/chunks/pages/index-120a18287eb7b315.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
