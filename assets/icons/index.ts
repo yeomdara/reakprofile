b85c4cb191daba999ceeb717044309c79e094abd
Reak Under <108196667+yeomdara@users.noreply.github.com>
2022-07-03 17:19:00 +0700

    initialcommit

---
+++ assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { GitHubIcon } from './Github.icon';
+export { GitlabIcon } from './Gitlab.icon';
+export { LinkedInIcon } from './LinkedIn.icon';
+export { ResumeIcon } from './Resume.icon';
+export { EmailIcon } from './Email.icon';
+export { DownloadIcon } from './Download.icon';
---
+++ components/ActionsList/actions-list.component.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { EmailIcon, LinkedInIcon, GitHubIcon, DownloadIcon, GitlabIcon } from "../../assets/icons";
+
+import styles from "./actions-list.module.scss";
+
+export function ActionsList(): JSX.Element {
+  const DATA = [
+    {
+      href: "mailto:moulhengborann@gmail.com",
+      aria: "Open a pre-addressed email prompt to me that you can fill out",
+      icon: <EmailIcon />,
+      label: "Email",
+    },
+    {
+      href: "https://www.linkedin.com",
+      aria: "Visit my LinkedIn profile to learn more about my education and work experience",
+      icon: <LinkedInIcon />,
+      label: "LinkedIn",
+    },
+    {
+      href: "https://github.com/yeomdara",
+      aria: "Visit my GitHub profile to learn more about the projects I have created and contributed to",
+      icon: <GitHubIcon />,
+      label: "Github",
+    },
+    {
+      href: "https://gitlab.com",
+      aria: "Visit my Gitlab profile to learn more about the projects I have created and contributed to",
+      icon: <GitlabIcon />,
+      label: "Github",
+    },
+    {
+      href: "",
+      aria: "Download my CV.",
+      icon: <DownloadIcon />,
+      label: "Download",
+    },
+  ];
+
+  const Button = ({ href, aria, icon, label }: { href: string; aria: string; icon: React.ReactNode; label: string }) => {
+    return (
+      <span className={styles.buttonContainer}>
+        <a
+          className={styles.button}
+          download={label === "Download" ? href : ""}
+          href={href}
+          target="_blank"
+          aria-label={aria}
+          rel="noopener noreferrer"
+        >
+          <div className={styles.icon}>{icon}</div>
+          <span className={styles.iconTitle}>{label}</span>
+        </a>
+      </span>
+    );
+  };
+
+  return (
+    <div>
+      {DATA.map((props, i) => (
+        <Button {...props} key={i} />
+      ))}
+    </div>
+  );
+}
